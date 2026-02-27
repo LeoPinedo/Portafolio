f4b575c73340fc2ea56630368656b2dc4641f8a0
LeoPinedo <lionelmessi8345@gmail.com>
2026-02-26 22:38:51 -0500

    changes 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH || ''
 const nextConfig = {
   output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  // Si tu repo NO se llama "leopinedo.github.io" sino ej. "Portafolio", descomenta y usa el nombre del repo:
-  // basePath: process.env.GITHUB_PAGES === 'true' ? '/Portafolio' : '',
-  // assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/Portafolio/' : '',
 }
 
 export default nextConfig
